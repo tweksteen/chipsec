36473dae2c63ba10960533612ff2ac12fa3abfd6
Thiebaud Weksteen <tweek@google.com>
2015-11-02 11:13:33 +1100

    Change io command to a class

--- source/tool/chipsec/utilcmd/io_cmd.py
+++ source/tool/chipsec/utilcmd/io_cmd.py
@@ -27,18 +27,10 @@ The io command allows direct access to read and write I/O port space.
 
 __version__ = '1.0'
 
-import os
-import sys
-import time
-
-import chipsec_util
-
-from chipsec.logger     import *
-from chipsec.file       import *
-
+from chipsec.command import BaseCommand
 
 # Port I/O
-def port_io(argv):
+class PortIOCommand(BaseCommand):
     """
     >>> chipsec_util io <io_port> <width> [value]
 
@@ -47,50 +39,56 @@ def port_io(argv):
     >>> chipsec_util io 0x61 1
     >>> chipsec_util io 0x430 byte 0x0
     """
-    if 3 > len(argv):
-        print port_io.__doc__
-        return
+    def requires_driver(self):
+        if len(self.argv) < 3:
+            return False
+        return True
 
-    try:
-        io_port = int(argv[2],16)
+    def run(self):
+        if 3 > len(self.argv):
+            print PortIOCommand.__doc__
+            return
 
-        if 3 == len(argv):
-            width = 1
-        else:
-            if 'byte' == argv[3]:
+        try:
+            io_port = int(self.argv[2],16)
+
+            if 3 == len(self.argv):
                 width = 1
-            elif 'word' == argv[3]:
-                width = 2
-            elif 'dword' == argv[3]:
-                width = 4
             else:
-                width = int(argv[3])
-    except:
-        print port_io.__doc__
-        return
-
-    if 5 == len(argv):
-        value = int(argv[4], 16)
-        logger().log( "[CHIPSEC] OUT 0x%04X <- 0x%08X (size = 0x%02x)" % (io_port, value, width) )
-        if 1 == width:
-            chipsec_util._cs.io.write_port_byte( io_port, value )
-        elif 2 == width:
-            chipsec_util._cs.io.write_port_word( io_port, value )
-        elif 4 == width:
-            chipsec_util._cs.io.write_port_dword( io_port, value )
-        else:
-            print "ERROR: Unsupported width 0x%x" % width
+                if 'byte' == self.argv[3]:
+                    width = 1
+                elif 'word' == self.argv[3]:
+                    width = 2
+                elif 'dword' == self.argv[3]:
+                    width = 4
+                else:
+                    width = int(self.argv[3])
+        except:
+            print PortIOCommand.__doc__
             return
-    else:
-        if 1 == width:
-            value = chipsec_util._cs.io.read_port_byte( io_port )
-        elif 2 == width:
-            value = chipsec_util._cs.io.read_port_word( io_port )
-        elif 4 == width:
-            value = chipsec_util._cs.io.read_port_dword( io_port )
-        else:
-            print "ERROR: Unsupported width 0x%x" % width
-            return
-        logger().log( "[CHIPSEC] IN 0x%04X -> 0x%08X (size = 0x%02x)" % (io_port, value, width) )
 
-chipsec_util.commands['io'] = {'func' : port_io, 'start_driver' : True, 'help' : port_io.__doc__  }
+        if 5 == len(self.argv):
+            value = int(self.argv[4], 16)
+            self.logger.log( "[CHIPSEC] OUT 0x%04X <- 0x%08X (size = 0x%02x)" % (io_port, value, width) )
+            if 1 == width:
+                self.cs.io.write_port_byte( io_port, value )
+            elif 2 == width:
+                self.cs.io.write_port_word( io_port, value )
+            elif 4 == width:
+                self.cs.io.write_port_dword( io_port, value )
+            else:
+                print "ERROR: Unsupported width 0x%x" % width
+                return
+        else:
+            if 1 == width:
+                value = self.cs.io.read_port_byte( io_port )
+            elif 2 == width:
+                value = self.cs.io.read_port_word( io_port )
+            elif 4 == width:
+                value = self.cs.io.read_port_dword( io_port )
+            else:
+                print "ERROR: Unsupported width 0x%x" % width
+                return
+            self.logger.log( "[CHIPSEC] IN 0x%04X -> 0x%08X (size = 0x%02x)" % (io_port, value, width) )
+
+commands = { 'io': PortIOCommand }
