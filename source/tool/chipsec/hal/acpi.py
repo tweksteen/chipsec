0b4b7e2d6058d32379cccad387b9ee2abb6f5aed
Thiebaud Weksteen <tweek@google.com>
2016-03-11 14:49:43 +1100

    Fix the parsing of RSDT and RSDP

--- source/tool/chipsec/hal/acpi.py
+++ source/tool/chipsec/hal/acpi.py
@@ -163,32 +163,39 @@ RSDP_GUID_ACPI1_0 = 'EB9D2D31-2D88-11D3-9A16-0090273FC14D'
 
 ACPI_RSDP_SIG = 'RSD PTR '
 # RSDP Format
-ACPI_RSDP_FORMAT = '=8sB6sBIIQB3s'
-ACPI_RSDP_SIZE   = struct.calcsize(ACPI_RSDP_FORMAT)
-assert( 36 == ACPI_RSDP_SIZE )
+ACPI_RSDP_FORMAT = '<8sB6sBI'
+ACPI_RSDP_EXT_FORMAT = 'IQB3s'
+ACPI_RSDP_SIZE = struct.calcsize(ACPI_RSDP_FORMAT)
+ACPI_RSDP_EXT_SIZE = struct.calcsize(ACPI_RSDP_FORMAT + ACPI_RSDP_EXT_FORMAT)
+assert ACPI_RSDP_EXT_SIZE == 36
 
 class RSDP():
     __slots__ = ()
     def __init__( self, table_content ):
-        self.ACPI_RSDP_FORMAT = '=8sB6sBIIQB3s'
-        (self.Signature, self.Checksum, self.OEMID, self.Revision, self.RsdtAddress, self.Length, self.XsdtAddress, self.ExtChecksum, self.Reserved) = struct.unpack( self.ACPI_RSDP_FORMAT, table_content )
+        if len(table_content) == ACPI_RSDP_SIZE:
+          (self.Signature, self.Checksum, self.OEMID,
+           self.Revision, self.RsdtAddress) = struct.unpack(ACPI_RSDP_FORMAT, table_content)
+        else:
+          (self.Signature, self.Checksum, self.OEMID,
+           self.Revision, self.RsdtAddress, self.Length,
+           self.XsdtAddress, self.ExtChecksum, self.Reserved) = struct.unpack(ACPI_RSDP_FORMAT + ACPI_RSDP_EXT_FORMAT, table_content)
     def __str__( self ):
-        return """==================================================================
-  Root System Description Pointer (RSDP)
-==================================================================
-  Signature        : %s
-  Checksum         : 0x%02X
-  OEM ID           : %s
-  Revision         : 0x%02X
-  RSDT Address     : 0x%08X
-  Length           : 0x%08X
-  XSDT Address     : 0x%016X
-  Extended Checksum: 0x%02X
-  Reserved         : %s
-""" % ( self.Signature, self.Checksum, self.OEMID, self.Revision, self.RsdtAddress, self.Length, self.XsdtAddress, self.ExtChecksum, self.HEX_STRING(self.Reserved) )
-    
-    def HEX_STRING( self, _str ):
-        return (''.join('%02x ' % ord(c) for c in _str))
+        default = ("==================================================================\n"
+                   "  Root System Description Pointer (RSDP)\n"
+                   "==================================================================\n"
+                   "  Signature        : %s\n"
+                   "  Checksum         : 0x%02X\n"
+                   "  OEM ID           : %s\n"
+                   "  Revision         : 0x%02X\n"
+                   "  RSDT Address     : 0x%08X\n"
+                  ) % (self.Signature, self.Checksum, self.OEMID, self.Revision, self.RsdtAddress)
+        if hasattr(self, "Length"):
+          default += ("  Length           : 0x%08X\n"
+                      "  XSDT Address     : 0x%016X\n"
+                      "  Extended Checksum: 0x%02X\n"
+                      "  Reserved         : %s\n"
+                     ) % (self.Length, self.XsdtAddress, self.ExtChecksum, self.Reserved.encode("hex"))
+        return default
 
     # some sanity checking on RSDP
     def is_RSDP_valid(self ):
@@ -299,7 +306,7 @@ class ACPI:
     #
     def get_SDT( self, search_rsdp=True ):
         if search_rsdp:
-            (rsdp_pa,rsdp) = self.find_RSDP()
+            (rsdp_pa, rsdp) = self.find_RSDP()
             if 0x0 == rsdp.Revision:
                 sdt_pa = rsdp.RsdtAddress
                 is_xsdt = False
@@ -308,7 +315,7 @@ class ACPI:
                 is_xsdt = True
             else:
                 return (False,None,None,None)
-            logger().log( "[acpi] found %s at PA: 0x%016X" % ('XSDT' if is_xsdt else 'SSDT', sdt_pa) )
+            logger().log( "[acpi] found %s at PA: 0x%016X" % ('XSDT' if is_xsdt else 'RSDT', sdt_pa) )
             sdt_header_buf = self.cs.mem.read_physical_mem( sdt_pa, ACPI_TABLE_HEADER_SIZE )
             sdt_header     = self._parse_table_header( sdt_header_buf )
             sdt_buf        = self.cs.mem.read_physical_mem( sdt_pa, sdt_header.Length )
